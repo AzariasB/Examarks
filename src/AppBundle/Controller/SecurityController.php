303d43c6aea823973eee11b073d4d4b2d8902903
AzariasB <azarias.boutin@gmail.com>
2017-07-11 19:18:30 +0100

    Basic login form for root

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 azarias.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Description of SecurityController
+ *
+ * @author azarias
+ */
+class SecurityController extends Controller {
+    //put your code here
+    
+    /**
+     * @Route("/login", name = "login")
+     */
+    public function loginAction(Request $req, AuthenticationUtils $authUtils){
+        
+        $error = $authUtils->getLastAuthenticationError();
+        
+        $lastUsername = $authUtils->getLastUsername();
+        
+        return $this->render('login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User implements \Symfony\Component\Security\Core\User\UserInterface, \Symfony\Component\Security\Core\User\EquatableInterface, \JsonSerializable {
+
+    const ROLE_USER = 1;
+    const ROLE_STUDENT = 2;
+    const ROLE_TEACHER = 4;
+    const ROLE_ADMIN = 8;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=255, unique=true)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     *
+     * @var int
+     * @ORM\Column(name="roles", type="integer")
+     */
+    private $roles;
+
+    public function __construct(string $login, string $password, array $roles) {
+        $this->login = $login;
+        $this->password = $password;
+        $this->setRoles($roles);
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     *
+     * @return User
+     */
+    public function setLogin($login) {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string
+     */
+    public function getLogin() {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password) {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword() {
+        return $this->password;
+    }
+
+    /**
+     * Set new role
+     * 
+     * @param int $nwRoles
+     */
+    public function setRoles($nwRoles) {
+        if (!is_array($nwRoles)) {
+            $this->roles = $nwRoles;
+            return;
+        }
+
+        foreach ($nwRoles as $r) {
+            if ($r == "ROLE_USER") {
+                $this->roles |= User::ROLE_USER;
+            } else if ($r === "ROLE_STUDENT") {
+                $this->roles |= User::ROLE_STUDENT;
+            } else if ($r === "ROLE_TEACHER") {
+                $this->roles |= User::ROLE_TEACHER;
+            } else if ($r === "ROLE_ADMIN") {
+                $this->roles |= User::ROLE_ADMIN;
+            }
+        }
+    }
+
+    public function eraseCredentials() {
+        //Nothing sensitive to erase
+    }
+
+    public function getRoles() {
+        $res = [];
+        if ($this->roles & User::ROLE_USER) {
+            $res[] = "ROLE_USER";
+        }
+        if ($this->roles & User::ROLE_STUDENT) {
+            $res[] = "ROLE_STUDENT";
+        }
+        if ($this->roles & User::ROLE_TEACHER) {
+            $res[] = "ROLE_TEACHER";
+        }
+        if ($this->roles & User::ROLE_ADMIN) {
+            $res[] = "ROLE_ADMIN";
+        }
+
+        return $res;
+    }
+
+    public function getSalt() {
+        return hash("sha256", $this->login);
+    }
+
+    public function getUsername(): string {
+        return $this->login;
+    }
+
+    public function isEqualTo(\Symfony\Component\Security\Core\User\UserInterface $user): bool {
+        return $user->getUsername() == $this->getUsername();
+    }
+
+    public function jsonSerialize() {
+        return [
+            "id" => $this->id,
+            "login" => $this->login
+        ];
+    }
+
+}
